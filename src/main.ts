96150bd86c1dcc13b0e21d8102df4e9d9b2fe8f2
haha58 <1461089222@qq.com>
2025-07-17 23:03:35 +0800

    -显示具体的错信息

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,31 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import './assets/main.css'
 import ErrorStackParser from 'error-stack-parser'
-import { findCodeBySourceMap } from './utils/index'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus'
+import App from './App.vue'
+import router from './router'
+// import { findCodeBySourceMap } from './utils/index'
+const app = createApp(App)
+app.use(ElementPlus)
+app.use(createPinia())
+app.use(router)
+// import { findCodeBySourceMap } from './utils/index'
 
-const app=createApp(App)
-
-app.config.errorHandler=(err,vm)=>{
-    const errorStack=ErrorStackParser.parse(err as Error)
+app.config.errorHandler=(err:Error,vm?:any)=>{
+    const errorStack = ErrorStackParser.parse(err as Error)
+    const jsError = {
+        stack_frames: errorStack,
+        message: err.message,
+        stack: err.stack,
+        error_name: err.name
+    }
+    vm!.$message.error(`您触发了一个${err.name} 错误`)
+    localStorage.setItem('jsErrorList', JSON.stringify(jsError))
     console.log("errorStack",errorStack)
     //错误栈中第一个才是报错的位置
-    findCodeBySourceMap(errorStack[0])
+    // findCodeBySourceMap(errorStack[0])
 }
 
 app.mount('#app')
