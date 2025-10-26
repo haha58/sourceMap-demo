514afd866dd641095d2449332923328c76f683e5
haha58 <haha58@users.noreply.github.com>
2025-10-26 08:19:07 +0000

    deploy: 76f289d8acb1095ea4815b4e4d262a291df5331a

---
+++ assets/eventStore-BoblpEz0.js
@@ -0,0 +1,2 @@
+import{g as t}from"./index-BUxw4t8j.js";const n=t("eventStore",{state:()=>({events:[]}),getters:{},actions:{setEvents(e){this.events=e},getEvents(){return this.events}}});export{n as u};
+//# sourceMappingURL=eventStore-BoblpEz0.js.map
